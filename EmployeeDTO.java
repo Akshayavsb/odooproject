7e33c492784904a8bd85fb673149febbda1c5e55
harshini565 <thangavelp1983@gmail.com>
2026-08-22 13:35:21 +0530

    Add files via upload

---
+++ EmployeeDTO.java
@@ -0,0 +1,71 @@
+package com.dayflow.hrms.dto;
+
+public class EmployeeDTO {
+
+    private Long id;
+    private String name;
+    private String department;
+    private String designation;
+    private String phone;
+    private String address;
+    private double salary;
+
+    public EmployeeDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
