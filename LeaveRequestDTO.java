2eab77cd681cecc5f02b6a7258f6385e6025b296
harshini565 <thangavelp1983@gmail.com>
2026-08-22 13:34:21 +0530

    Add files via upload

---
+++ LeaveRequestDTO.java
@@ -0,0 +1,73 @@
+package com.dayflow.hrms.dto;
+
+import java.time.LocalDate;
+
+public class LeaveRequestDTO {
+
+    private Long id;
+    private Long employeeId;
+    private String leaveType;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String remarks;
+    private String status;
+
+    public LeaveRequestDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getLeaveType() {
+        return leaveType;
+    }
+
+    public void setLeaveType(String leaveType) {
+        this.leaveType = leaveType;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
