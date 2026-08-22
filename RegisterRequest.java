5e6f9d5ec379c1e6012c064ae2732cd8bf5227e9
harshini565 <thangavelp1983@gmail.com>
2026-08-22 13:35:39 +0530

    Add files via upload

---
+++ RegisterRequest.java
@@ -0,0 +1,44 @@
+package com.dayflow.hrms.dto;
+
+public class RegisterRequest {
+
+    private String employeeId;
+    private String email;
+    private String password;
+    private String role;
+
+    public RegisterRequest() {
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
