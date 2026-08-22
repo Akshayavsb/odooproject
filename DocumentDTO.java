519b5f91f2d241c6060fe38bf894d285a4cb39e8
harshini565 <thangavelp1983@gmail.com>
2026-08-22 13:33:27 +0530

    Add files via upload

---
+++ DocumentDTO.java
@@ -0,0 +1,44 @@
+package com.dayflow.hrms.dto;
+
+public class DocumentDTO {
+
+    private Long employeeId;
+    private String documentName;
+    private String documentType;
+    private String documentUrl;
+
+    public DocumentDTO() {
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getDocumentName() {
+        return documentName;
+    }
+
+    public void setDocumentName(String documentName) {
+        this.documentName = documentName;
+    }
+
+    public String getDocumentType() {
+        return documentType;
+    }
+
+    public void setDocumentType(String documentType) {
+        this.documentType = documentType;
+    }
+
+    public String getDocumentUrl() {
+        return documentUrl;
+    }
+
+    public void setDocumentUrl(String documentUrl) {
+        this.documentUrl = documentUrl;
+    }
+}
