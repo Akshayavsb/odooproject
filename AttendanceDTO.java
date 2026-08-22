8838e86c29446a4735bed050f453be1a1475bf39
harshini565 <thangavelp1983@gmail.com>
2026-08-22 13:34:54 +0530

    Add files via upload

---
+++ AttendanceDTO.java
@@ -0,0 +1,46 @@
+package com.dayflow.hrms.dto;
+
+import java.time.LocalDate;
+
+public class AttendanceDTO {
+
+    private Long id;
+    private Long employeeId;
+    private LocalDate date;
+    private String status;
+
+    public AttendanceDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
